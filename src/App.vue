eba4ca595839698df58eacdac50b5d1b09838d4a
Carl-J-M <carlmensahmail@gmail.com>
2020-02-28 21:00:06 +0000

    Fetch request implemented, request formatting into array & ListItem component displayed to screen.

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,57 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="https://external-preview.redd.it/sAcvIebeiBWPryrpGDb7G4KaqgpRK3ZZ6dgozI__pK8.png?auto=webp&s=abd5e766710c80c8384da1d5fb0529f509986c01">
-    <h1>HELLO</h1>
+
+    <img id="logo" alt="Vue logo" src="https://external-preview.redd.it/sAcvIebeiBWPryrpGDb7G4KaqgpRK3ZZ6dgozI__pK8.png?auto=webp&s=abd5e766710c80c8384da1d5fb0529f509986c01">
+    <h1 id="home-heading">Page 1 of www.reddit.com/r/{{subreddit}}</h1>
+    <list-item :listItemData="listItemData"/>
+
   </div>
 </template>
 
 <script>
 
 
+import ListItem from './components/ListItem.vue'
+
 export default {
   name: 'App',
+  data() {
+    return {
+      pageData: {},
+      listItemData: [],
+      subreddit: ""
+    }
+},
   components: {
+    "list-item": ListItem
+    //
+  },
+  methods: {
+      formatThreads: function(threadArray) {
 
-// https://www.reddit.com/r/javascript.json
+        threadArray.forEach((thread) => {
+          let newArray = [];
+          newArray.push(thread.data);
+          this.listItemData.push(newArray);
+        })
+
+      },
+      getSubreddit: function(threadArray) {
+      this.subreddit = threadArray[0].data.subreddit;
+
+      }
+  },
+
+  mounted() {
+    fetch('https://www.reddit.com/r/globaloffensive.json')
+    .then(response => response.json())
+    .then((pageData) => {
+      this.pageData = pageData;
+      this.formatThreads(pageData.data.children);
+      this.getSubreddit(pageData.data.children);
+    })
+
+
   }
 }
 </script>
@@ -22,8 +61,16 @@ export default {
   font-family: Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+
   color: #2c3e50;
   margin-top: 60px;
+}
+#logo {
+height: 7em;
+width: auto;
+margin-left: 40%;
+}
+#home-heading {
+  text-align: center;
 }
 </style>
--- src/components/ListItem.vue
+++ src/components/ListItem.vue
@@ -1,11 +1,25 @@
 <template lang="html">
 
+  <ul>
+    <li v-for="thread in listItemData" class="thread">
+      {{thread[0].title}}
+    </li>
+  </ul>
 </template>
 
 <script>
 export default {
+  name: 'list-item',
+  props: ['listItemData'],
 }
+
 </script>
 
 <style lang="css" scoped>
+.thread {
+  margin-left: 20px;
+}
+.thread:hover {
+  color: blue;
+}
 </style>
