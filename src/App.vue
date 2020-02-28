9b4de56fc1a3d19b68d9a1305d757b568f82cd2f
Carl-J-M <carlmensahmail@gmail.com>
2020-02-28 18:57:39 +0000

    Initial commit.

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,25 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="https://external-preview.redd.it/sAcvIebeiBWPryrpGDb7G4KaqgpRK3ZZ6dgozI__pK8.png?auto=webp&s=abd5e766710c80c8384da1d5fb0529f509986c01">
+    <h1>HELLO</h1>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+
+// https://www.reddit.com/r/javascript.json
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
