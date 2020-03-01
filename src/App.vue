1352f1fe38b7baca5e047e240ed71faedce656f8
Carl-J-M <carlmensahmail@gmail.com>
2020-03-01 23:07:55 +0000

    Added dynamic page query & styling

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,16 @@
   <div id="app">
 
     <img id="logo" alt="Vue logo" src="https://external-preview.redd.it/sAcvIebeiBWPryrpGDb7G4KaqgpRK3ZZ6dgozI__pK8.png?auto=webp&s=abd5e766710c80c8384da1d5fb0529f509986c01">
-    <h1 id="home-heading">Page 1 of www.reddit.com/r/{{subreddit}}</h1>
+
+<form class="" action="index.html" method="post">
+      <h1 id="home-heading">www.reddit.com/r/
+        <input id="example" name="subreddit-input" v-model="subreddit" v-on:change="getNewPageData(subreddit)" autofocus/>
+
+      <h3 id="current-sub-header">Currently Browsing /r/{{subreddit}}</h3>
+    </h1>
+
+</form>
+
     <list-item :listItemData="listItemData"/>
 
   </div>
@@ -22,6 +31,9 @@ export default {
       subreddit: ""
     }
 },
+computed: {
+
+  },
   components: {
     "list-item": ListItem
     //
@@ -33,17 +45,29 @@ export default {
           let newArray = [];
           newArray.push(thread.data);
           this.listItemData.push(newArray);
-        })
 
+        })
       },
       getSubreddit: function(threadArray) {
       this.subreddit = threadArray[0].data.subreddit;
 
+    },
+      getNewPageData: function(subreddit) {
+        this.listItemData = [];
+        let newRequest = `https://www.reddit.com/r/${subreddit}/.json`;
+        console.log(newRequest);
+          fetch(newRequest)
+          .then(response => response.json())
+          .then((pageData) => {
+            this.pageData = pageData;
+            this.formatThreads(pageData.data.children);
+          })
+          console.log("After getNewPageData called:", this.pageData);
       }
   },
 
   mounted() {
-    fetch('https://www.reddit.com/r/globaloffensive.json')
+    fetch(`https://www.reddit.com/r/iama/.json`)
     .then(response => response.json())
     .then((pageData) => {
       this.pageData = pageData;
@@ -68,9 +92,33 @@ export default {
 #logo {
 height: 7em;
 width: auto;
-margin-left: 40%;
+display: block;
+margin-left: auto;
+margin-right: auto;
 }
 #home-heading {
   text-align: center;
+  border-bottom: solid lightgray 2px;
+  padding-bottom: 20px;
+}
+
+#example {
+  font-family: Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  font-size: 24px;
+  color: gray;
+  border: none;
+  /* border-bottom: solid #2c3e50 2px; */
+  margin-top: 3px;
+  margin-left: 10px;
+  position: absolute;
+
+
+
+}
+#current-sub-header {
+  color: red;
+  font-size: 18px;
 }
 </style>
--- src/components/ListItem.vue
+++ src/components/ListItem.vue
@@ -1,8 +1,13 @@
 <template lang="html">
 
-  <ul>
+  <ul id="container">
     <li v-for="thread in listItemData" class="thread">
-      {{thread[0].title}}
+      <h2 id="thread-title"><a class="nostyle" :href="thread[0].url">{{thread[0].title}}</a></h2>
+      <p>Upvotes:<div class="bold">{{thread[0].ups}}</div></p>
+      <p>Author: <div class="bold">{{thread[0].author}}</div></p>
+      <p id="text">{{thread[0].selftext.substring(0, 400)}} ...</p>
+      <a id="reddit-link" :href="thread[0].url">View on Reddit</a>
+
     </li>
   </ul>
 </template>
@@ -18,8 +23,55 @@ export default {
 <style lang="css" scoped>
 .thread {
   margin-left: 20px;
+  margin-bottom: 30px;
+    list-style-type: none;
 }
-.thread:hover {
-  color: blue;
+#reddit-link {
+
+  color: #2c3e50;
+  font-weight: bold;
+  text-decoration: none;
+  border-bottom: solid red 2px;
+  padding: 5px 10px;
+  margin-top: 20px;
+
+
+}
+
+#reddit-link:hover {
+
+  color: red;
+
+
+}
+
+#thread-title {
+
+max-width: 45%;
+text-align: center;
+border-bottom: solid red 2px;
+margin-top: 40px;
+
+
+}
+#thread-title:hover {
+color: red;
+border-radius: 5px;
+}
+
+#container {
+
+}
+
+#text {
+  padding: 30px;
+}
+.bold {
+  font-weight: bold;
+  display: inline-block;
+}
+.nostyle{
+  color: inherit;
+  text-decoration: inherit;
 }
 </style>
